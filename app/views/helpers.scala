7b87cc9b34263ab79f678813610a8475d16fb864
Victor Basso <vitobasso@gmail.com>
2016-08-19 12:44:48 +0200

    list page

--- app/views/helpers.scala
+++ app/views/helpers.scala
@@ -14,13 +14,20 @@ object helpers {
     html.b3.text(form(fieldId), '_label -> Messages(fieldId))
 
   def checkbox[T](form: Form[T], fieldId: String)
-             (implicit messages: Messages, fc: B3FieldConstructor) =
+                 (implicit messages: Messages, fc: B3FieldConstructor) =
     html.b3.checkbox(form(fieldId), '_label -> Messages(fieldId))
 
   def select[T](form: Form[T], fieldId: String, options: Seq[String])
-             (implicit messages: Messages, fc: B3FieldConstructor) = {
+               (implicit messages: Messages, fc: B3FieldConstructor) = {
     def idAndLabel(optionId: String) = (optionId, Messages(s"$fieldId.$optionId"))
     html.b3.select(form(fieldId), options map idAndLabel)
   }
+
+  /**
+    * Workaround play templates not allowing type param
+    */
+  def table[T](items: Seq[T])(headers: Seq[String], toRow: T => Seq[String])
+              (implicit messages: Messages) =
+    html.table(headers, items.map(toRow(_)))
 
 }
